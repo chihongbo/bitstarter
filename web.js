dc640de1691e1e09d00209f79eeaffae22dd7dff
hchi <chihongbo@gmail.com>
2013-07-21 12:38:05 -0700

    Updated web.js again

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buf=new buffer(256);
+  buf=fs.readFileSync("index.html"); 
+  response.send('Hello World'+buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
