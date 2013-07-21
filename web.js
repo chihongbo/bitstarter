b3377d7fc43a0e0fbcfb6892db43ff51fc5b5e7b
hchi <chihongbo@gmail.com>
2013-07-21 12:49:31 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+var fs=require('fs');
+ buf=new Buffer(256);
+ buf=fs.readFileSync("index.html");
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  buf=new Buffer(256);
-  buf=fs.readFileSync("index.html"); 
+  //buf=new Buffer(256);
+  //buf=fs.readFileSync("index.html"); 
   response.send('Hello World'+buf.toString('utf-8'));
 });
 
